bea2391afaea7613afe65fa8a01406a9d29c2972
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2023-02-09 02:55:29 +0000

    Debug

--- tests/issue582.py
+++ tests/issue582.py
@@ -1,4 +1,12 @@
 import ujson
 
 for _ in range(1000):
-	print('ok' if ujson.loads(bytearray(b"99")) == 99 else 'ohno')
+	assert ujson.loads(b"123") == 123
+	try:
+		ujson.loads(memoryview(b"{}"))
+	except TypeError as e:
+		if 'PyPy' not in str(e):
+			raise
+	else:
+		raise RuntimeError('TypeError not raised')
+	assert ujson.loads(bytearray(b"99")) == 99
